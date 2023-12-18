2839878be427cae24798d6abfa97cdd3ece3e64a
Zartab M Nakhwa <zartab@codewithz.com>
2023-12-18 10:53:50 +0530

    Rust Syntax

---
+++ 1-getting-started/src/main.rs
@@ -0,0 +1,10 @@
+use std::io::stdin;
+
+fn main() {
+    let mut message=String::new();
+    println!("Enter your message:");
+
+    stdin().read_line(&mut message).unwrap();
+    println!("Message Recieved: {}",message);
+
+}
