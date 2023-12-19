e8ef32e04d700303ec8961fda61aff78ede580d2
Zartab M Nakhwa <zartab@codewithz.com>
2023-12-19 09:47:09 +0530

    References and Slices

--- ref-and-slices/src/main.rs
+++ ref-and-slices/src/main.rs
@@ -39,6 +39,35 @@ fn main() {
      let mut my_coffee=Coffee{id:10,count:50};
      alter_coffee(&mut my_coffee, 25);
      print_coffee(&my_coffee);
+
+// Slices -- arrays and string 
+
+let str_slice="Hello World";
+
+let my_str=String::from(str_slice);
+
+let slice_from_str= &my_str[0..5];
+println!("{}",slice_from_str);
+
+let my_arr=[1,2,3,4,5];
+let arr_slice=&my_arr[1..3];
+println!("My Array Slice {:?}",arr_slice);
+
+let mut my_mut_arr=[1,2,3,4,5];
+
+let  mut_array_slice=&mut my_mut_arr[0..3];
+println!("My Mutable Array Slice before modification {:?}",mut_array_slice);
+mut_array_slice[0]=1000;
+
+println!("My Mutable Array Slice after modification {:?}",mut_array_slice);
+println!("My Mutable Array  {:?}",my_mut_arr);
+
+let  mut_array_slice1=&mut my_mut_arr[1..4];
+mut_array_slice1[0]=43000;
+println!("My Mutable Array Slice after modification {:?}",mut_array_slice1);
+// If we uncomment below line it will start giving errors
+// println!("My Mutable Array Slice after modification {:?}",mut_array_slice);
+
 }
 
 // Macros imported by default
