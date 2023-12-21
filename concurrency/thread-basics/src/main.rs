6d17fcbd50da8434ce94b0a7836b3f3ece2373ae
Zartab M Nakhwa <zartab@codewithz.com>
2023-12-21 14:41:39 +0530

    Thread Moving Data

--- concurrency/thread-basics/src/main.rs
+++ concurrency/thread-basics/src/main.rs
@@ -3,18 +3,28 @@ use std::{thread, time::Duration};
 
 fn main() {
   
-  let handle=  thread::spawn(||{
-       for i in 1..10{
-        println!("Number : {} from a spawned thread",i);
-        thread::sleep(Duration::from_secs(1));
-       }
-    });
-    handle.join().unwrap();
+//   let handle=  thread::spawn(||{
+//        for i in 1..10{
+//         println!("Number : {} from a spawned thread",i);
+//         thread::sleep(Duration::from_secs(1));
+//        }
+//     });
+//     handle.join().unwrap();
 
-    for i in 1..5 {
-        println!("Number : {} from main thread!!",i);
-        thread::sleep(Duration::from_secs(1));
-    }
+//     for i in 1..5 {
+//         println!("Number : {} from main thread!!",i);
+//         thread::sleep(Duration::from_secs(1));
+//     }
+
+        let v=vec![1,2,3,4,5];
+
+        let handle=thread::spawn(move ||{
+            print!("Here is the value of vector {:?}",v);
+        });
+
+        // drop(v); Not allowed here
+
+        handle.join().unwrap();
 
     
 
