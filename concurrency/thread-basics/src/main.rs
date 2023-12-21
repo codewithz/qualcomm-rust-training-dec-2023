3f3013e68721dadccbd5b25302a2f9dc8071bc29
Zartab M Nakhwa <zartab@codewithz.com>
2023-12-21 15:24:54 +0530

    Reference Counter

--- concurrency/thread-basics/src/main.rs
+++ concurrency/thread-basics/src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time::Duration};
+use std::{thread, time::Duration, rc::Rc};
 
 
 fn main() {
@@ -16,16 +16,67 @@ fn main() {
 //         thread::sleep(Duration::from_secs(1));
 //     }
 
-        let v=vec![1,2,3,4,5];
+        // let v=vec![1,2,3,4,5];
 
-        let handle=thread::spawn(move ||{
-            print!("Here is the value of vector {:?}",v);
-        });
+        // let handle=thread::spawn(move ||{
+        //     print!("Here is the value of vector {:?}",v);
+        // });
 
-        // drop(v); Not allowed here
+        // // drop(v); Not allowed here
 
-        handle.join().unwrap();
+        // handle.join().unwrap();
+
+        // let one=thread::spawn(||{
+        //     println!("Logging from Thread 1 !!");
+        // });
+
+        // // Use of Thread builder 
+
+        // let two =thread
+        //                     ::Builder
+        //                     ::new()
+        //                     .name("Thread 2".to_string())
+        //                     .spawn(||{
+        //                         println!("Logging from Thread 2!");
+        //                     }).unwrap();
+                            
+        // let three=thread::spawn(||{
+        //     // Capture the underlying THread Object
+
+        //     let thread_two=two.thread();
+        //     println!("Thread 2 Name  and ID : {:?}/{:?}",
+        //                 thread_two.name(),
+        //                 thread_two.id());
+
+        //     two.join().unwrap();
+        //     println!("Logging from Thread 3!")
+        // });
+
+        // // Join the thread handdles 
+
+        // one.join().unwrap();
+        // three.join().unwrap();
+
+        let mut data=Rc::new(  vec![1,2,3]);
+        
+      
+
+        for i in 0..3{
+            // Create a new owned reference 
+            let data_ref=data.clone();
+            thread::spawn(move ||{
+                data_ref[0]+=i;
+            });
+        }
 
     
 
 }
+
+
+// `Rc<Vec<i32>>` cannot be sent between threads safely
+// within `{closure@src\main.rs:67:27: 67:34}`, the trait `Send` is not implemented for `Rc<Vec<i32>>`rustcClick for full compiler diagnostic
+
+// Reason : As the error message mentions, Rc cannot be sent between threads safely.
+//  This is because the internal reference count is not maintained in a thread-safe manner
+//  and can have a data race.
