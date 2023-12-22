ea02bc86127949146aa45334fead31c73a1ca486
Zartab M Nakhwa <zartab@codewithz.com>
2023-12-22 15:21:02 +0530

    Web Api POST

--- web-api/src/main.rs
+++ web-api/src/main.rs
@@ -1,4 +1,4 @@
-use warp::Filter;
+use warp::{http,Filter};
 use parking_lot::{RwLock};
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -26,15 +26,50 @@ impl Store{
     }
 }
 
+// Common Function 
+
+fn json_body()-> impl Filter<Extract = (Item,),Error = warp::Rejection> + Clone {
+    // When accepting the body, we want JSON Body
+    // (to reject huge payload)
+
+    warp::body::content_length_limit(1024*16).and(warp::body::json())
+}
+
+
+// FOr posting in List 
+
+async fn add_grocery_list_item(
+    item:Item,
+    store:Store
+) -> Result<impl warp::Reply,warp::Rejection>{
+    let r=store.grocery_list.read();
+    Ok(warp::reply::json(&*r))
+}
+
 #[tokio::main]
 async fn main() {
 
-    // GET  /hello/warp ==> 200 OK 
-    let hello=warp
-                            ::path!("hello" / String)
-                            .map(|name| format!("Hello, {}",name));
+    // // GET  /hello/warp ==> 200 OK 
+    // let hello=warp
+    //                         ::path!("hello" / String)
+    //                         .map(|name| format!("Hello, {}",name));
 
-    warp::serve(hello)
+    let store=Store::new();
+    let store_filter=warp
+                                            ::any()
+                                            .map(move || store.clone());
+
+
+    let add_items=warp::post()
+                                                               .and(warp::path("v1"))
+                                                               .and(warp::path("groceries"))
+                                                               .and(warp::path::end())
+                                                               .and(json_body())
+                                                               .and(store_filter.clone())
+                                                               .and_then(add_grocery_list_item);
+
+
+    warp::serve(add_items)
         .run(([127,0,0,1],3030))
         .await;
 }
